247d0046aa06a8363811388f334cdd6df7620455
Pankaj <omkargalaxi@gmail.com>
2022-06-29 08:48:55 +0530

    Update ResourceNotFoundException.java

--- src/main/java/com/example/Authenticator/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/Authenticator/exception/ResourceNotFoundException.java
@@ -1,6 +1,10 @@
 package com.example.Authenticator.exception;
 
-public class ResourceNotFoundException extends Exception {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)  
+public class ResourceNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = -9079454849611061074L;
 
